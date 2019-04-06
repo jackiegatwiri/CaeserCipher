55d46d641912b0772e2d9b60023ab15ecc820d78
jackiegatwiri <jaciegatz@gmail.com>
2019-04-06 20:29:29 +0300

    create a new instance of StringBuilder named decipher

--- src/main/java/CeaserCipher/Encrypt.java
+++ src/main/java/CeaserCipher/Encrypt.java
@@ -1,21 +1,21 @@
 package CeaserCipher;
 
 public class Encrypt {
-    public static StringBuilder encrypt(String text, int shift) {
+    public static StringBuilder encrypt(String story, int key) {
 
-        StringBuilder result = new StringBuilder();
+        StringBuilder decipher = new StringBuilder();
 
-        for (int i = 0; i < text.length(); i++) {
-            if (Character.isUpperCase(text.charAt(i))) {
-                char ch = (char) (((int) text.charAt(i) +
-                        shift - 65) % 26 + 65);
-                result.append(ch);
-            } else {
-                char ch = (char) (((int) text.charAt(i) +
-                        shift - 97) % 26 + 97);
-                result.append(ch);
-            }
-        }
-        return result;
-    }
+//        for (int i = 0; i < text.length(); i++) {
+//            if (Character.isUpperCase(text.charAt(i))) {
+//                char ch = (char) (((int) text.charAt(i) +
+//                        shift - 65) % 26 + 65);
+//                result.append(ch);
+//            } else {
+//                char ch = (char) (((int) text.charAt(i) +
+//                        shift - 97) % 26 + 97);
+//                result.append(ch);
+//            }
+//        }
+//        return result;
+//    }
 }
