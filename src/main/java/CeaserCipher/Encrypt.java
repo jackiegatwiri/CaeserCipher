25c01f793066b479782b7e62c31c0a337cfbc5bc
jackiegatwiri <jaciegatz@gmail.com>
2019-04-06 21:40:53 +0300

    add conditions to ensure key is between 26

--- src/main/java/CeaserCipher/Encrypt.java
+++ src/main/java/CeaserCipher/Encrypt.java
@@ -2,8 +2,13 @@ package CeaserCipher;
 
 public class Encrypt {
     public static StringBuilder encrypt(String story, int key) {
-
         StringBuilder decipher = new StringBuilder();
+        if (key>26){
+            key = key%26;
+        }
+        else if(key<0){
+            key =  (key%26)+26;
+        }
 
         for (int i = 0; i < story.length(); i++) {
             if (Character.isUpperCase(story.charAt(i))) {
