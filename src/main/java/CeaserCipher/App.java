93430fe2add27f2ded50cac6508a0af166ebc028
jackiegatwiri <jaciegatz@gmail.com>
2019-04-06 20:49:24 +0300

    import scanner to collect your user input

--- src/main/java/CeaserCipher/App.java
+++ src/main/java/CeaserCipher/App.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
-//        System.out.println("please enter your story");
-//        String text;
+        System.out.println("please enter your story");
+        String text;
 //        text = myScanner.nextLine();
 //        System.out.println("please enter your shift");
 //        int shift = myScanner.nextInt();
